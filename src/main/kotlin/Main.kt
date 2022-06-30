fb11ff7f48c8d12f3a60926eacdd4fb56b38d89c
TeterinMN <Scorpion.TMN@ya.ru>
2022-06-30 16:26:11 +0700

    Добавил код программы

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,3 +13,43 @@
  * значит 1% применяется к 95%
  */
 
+fun main() {
+    val itemPrise = 100
+    val itemCount = 301
+    val discountStart = 1001
+    val discountLarge = 10_000
+    var totalPrise = itemPrise * itemCount
+
+    if (totalPrise in (discountStart + 1) until discountLarge) {
+        println(
+            "Стоимость одной композиции = 100 руб. \nМы покупаем $itemCount композиций на сумму $totalPrise руб. " +
+                    "и получаем скидку в 100 руб."
+        )
+        totalPrise -= 100
+        println("Итого: $totalPrise рублей")
+    } else if (totalPrise > discountLarge && itemCount < 300) {
+        val discount = (totalPrise * 5) / 100
+        println(
+            "Стоимость одной композиции = 100 руб. \nМы покупаем $itemCount композиций на сумму $totalPrise руб. " +
+                    "и получаем скидку в 5%"
+        )
+        totalPrise -= discount
+        println("Итого: $totalPrise рублей c учетом скидки в 5%")
+    } else if (itemCount >= 300) {
+        val discount = (totalPrise * 5) / 100
+        println(
+            "Стоимость одной композиции = 100 руб. \nМы покупаем $itemCount композиций на сумму $totalPrise руб. " +
+                    "и получаем скидку в 5%, а так же 1% как постоянному покупателю"
+        )
+        totalPrise -= discount
+        val discount2: Double = ((totalPrise * 1) / 100).toDouble()
+        val res: Double = totalPrise - discount2
+        println("Итого: $res рублей c учетом скидки в 5% и 1%")
+    } else {
+        println(
+            "Стоимость одной композиции = 100 руб. \nМы покупаем $itemCount композиций на сумму $totalPrise руб. " +
+                    "СКИДОК НЕТ"
+        )
+    }
+
+}
